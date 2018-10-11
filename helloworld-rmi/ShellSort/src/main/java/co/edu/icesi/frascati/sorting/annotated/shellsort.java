3ef7d7e6d0a54a6e9a95dff712edf7f1540c9a31
garzuxo <garzuxo@gmail.com>
2018-10-11 16:24:19 -0500

    added shellsort-server

--- helloworld-rmi/ShellSort/src/main/java/co/edu/icesi/frascati/sorting/annotated/shellsort.java
+++ helloworld-rmi/ShellSort/src/main/java/co/edu/icesi/frascati/sorting/annotated/shellsort.java
@@ -1,49 +1,12 @@
-     import java.io.*;
-    import java.util.*;
+  
+package co.edu.icesi.frascati.sorting.annotated;
 
-public class shellsort{
-  static int sort(int arr[]) 
-    { 
-        int n = arr.length; 
-  
-        for (int gap = n/2; gap > 0; gap /= 2) 
-        { 
-           
-            for (int i = gap; i < n; i += 1) 
-            { 
-                
-                int temp = arr[i]; 
-  
-              
-                int j; 
-                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
-                    arr[j] = arr[j - gap]; 
-  
-               
-                arr[j] = temp; 
-            } 
-        } 
-        return 0; 
-    } 
-  static void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
+import org.osoa.sca.annotations.Service;
+public interface IDistSort{
 
- public static void main(String args[]) { 
-        int arr[] = {12, 34, 54, 2, 3}; 
-        System.out.println("Array before sorting"); 
-        printArray(arr); 
-  
-      
-       sort(arr); 
-  
-        System.out.println("Array after sorting"); 
-        printArray(arr); 
-    } 
+@Service
+String[] sort(String[] cadena);
 }
+
 
 
