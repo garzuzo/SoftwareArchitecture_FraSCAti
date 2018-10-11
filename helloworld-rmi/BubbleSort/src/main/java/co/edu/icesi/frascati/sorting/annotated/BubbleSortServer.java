4de3e011049079fc54c094dfba3e27dfb8a74b59
mishale22 <sami_na11@hotmail.com>
2018-10-11 16:18:48 -0500

    BubbleSortServer

--- helloworld-rmi/BubbleSort/src/main/java/co/edu/icesi/frascati/sorting/annotated/BubbleSortServer.java
+++ helloworld-rmi/BubbleSort/src/main/java/co/edu/icesi/frascati/sorting/annotated/BubbleSortServer.java
@@ -21,16 +21,41 @@
  * Contributor(s): Christophe Demarey
  *                 Nicolas Dolet
  *                 Philippe Merle
+ *
  */
-package org.ow2.frascati.examples.helloworld.annotated;
+package co.edu.icesi.frascati.sorting.annotated;
 
-import org.osoa.sca.annotations.Service;
-
+import org.osoa.sca.annotations.Property;
+import java.io.*;
+import java.util.*;
 /**
- * A basic service used to print messages. 
+ * The print service implementation.
  */
-@Service
-public interface PrintService
+public class BubbleSortServer implements PrintService
 {
-    void print(String msg);
+    public BubbleSortServer ()
+    {
+        System.out.println("BUBBLESORTSERVER created.");
+    }
+
+    /**
+     * BubbleSort implementation.
+     */
+    public static String[] sort(String[] cadena)
+    {
+       long start= System.currentTimeMillis(); 
+       int n = cadena.length; 
+        for (int i = 0; i < n-1; i++) 
+            for (int j = 0; j < n-i-1; j++) 
+                if (cadena[j].compareTo(cadena[j+1]) > 0) 
+                { 
+                    String temp = cadena[j]; 
+                    cadena[j] = cadena[j+1]; 
+                    cadena[j+1] = temp; 
+                } 
+	long end= System.currentTimeMillis(); 
+	System.out.println(end-start); 
+	return cadena;
+    }
+
 }
